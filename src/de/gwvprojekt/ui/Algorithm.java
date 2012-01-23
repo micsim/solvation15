6b863c6af3428745fa35d557eb077eb45477adde
Felix Kiehn <fekiehn@web.de>
2012-01-23 13:12:06 +0100

    - package de.gwvprojekt.ui erstellt - Interfaces Data.java und Algorithm.java 	diesem package hinzugefügt

--- src/de/gwvprojekt/ui/Algorithm.java
+++ src/de/gwvprojekt/ui/Algorithm.java
@@ -1,3 +1,4 @@
+package de.gwvprojekt.ui;
 
 public interface Algorithm
 {
--- src/de/gwvprojekt/ui/Data.java
+++ src/de/gwvprojekt/ui/Data.java
@@ -1,8 +1,9 @@
+package de.gwvprojekt.ui;
 
 
 public interface Data
 {
-	public String get(int row, int column);
+	public String getStringValue(int row, int column);
 
 	public void move(int row, int column);
 
