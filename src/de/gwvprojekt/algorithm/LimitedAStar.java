11838e27a6a3f369d819df9ff196309fb4cad999
Felix Kiehn <fekiehn@web.de>
2012-01-26 22:06:46 +0100

    noch mehr guizucker

--- src/de/gwvprojekt/algorithm/LimitedAStar.java
+++ src/de/gwvprojekt/algorithm/LimitedAStar.java
@@ -2,7 +2,6 @@ package de.gwvprojekt.algorithm;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class LimitedAStar extends AbstractAlgorithm{
